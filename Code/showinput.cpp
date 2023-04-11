59ae700a8e0aac1f9e38d3cf3dd276931bdc079b
WisdomerLee <50577439+WisdomerLee@users.noreply.github.com>
2023-04-11 09:24:23 +0900

    입력받은 것을 띄우기
    cin >> 함수를 이용하여 변수에 해당 값을 집어넣기

---
+++ Code/showinput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int favorate_number;
+
+	cout << "1과 100 사이의 좋아하는 숫자를 넣으세요:";
+	//입력받은 숫자를 favorate_number에 집어넣기
+	cin >> favorate_number;
+	cout << "놀랍군요!" << endl;
+	cout <<favorate_number << "는 저도 좋아하는 숫자예요";
+	return 0;
+}
